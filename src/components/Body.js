52a839259440e22ccafdc0dc1ce2f8e054c3159c
Harikrishna <podilapu.harikrishna@capgemini.com>
2024-02-27 17:23:00 +0530

    search filter functonality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,11 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
+import Shimmer from "./shimmer";
 import { useEffect, useState } from "react";
 
 const Body = () => {
   const [listofRestaurants, setlistofRestaurants] = useState([]);
-  // console.log(listofRestaurants);
+  const [searchText, setsearchText] = useState("");
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
   useEffect(() => {
     fetchData();
@@ -20,89 +21,41 @@ const Body = () => {
       json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
     );
 
+    //optional Chaining
     setlistofRestaurants(
-      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
 
-  //   let listofRestaurantsJS = [
-  //     {
-  //       info: {
-  //         id: "201223",
-  //         name: "Asha tiffins",
-  //         cloudinaryImageId: "n15vckntsiboiod3gpco",
-  //         locality: "HSR Layout",
-  //         areaName: "Hsr Layout",
-  //         costForTwo: "₹200 for two",
-  //         cuisines: [
-  //           "South Indian",
-  //           "North Indian",
-  //           "Chinese",
-  //           "Desserts",
-  //           "Beverages",
-  //         ],
-  //         avgRating: 4.5,
-  //         parentId: "236243",
-  //         avgRatingString: "4.5",
-  //         totalRatingsString: "10K+",
-  //         sla: {
-  //           deliveryTime: 26,
-  //         },
-  //       },
-  //     },
-  //     {
-  //       info: {
-  //         id: "201224",
-  //         name: "Uduppi",
-  //         cloudinaryImageId: "n15vckntsiboiod3gpco",
-  //         locality: "HSR Layout",
-  //         areaName: "Hsr Layout",
-  //         costForTwo: "₹200 for two",
-  //         cuisines: [
-  //           "South Indian",
-  //           "North Indian",
-  //           "Chinese",
-  //           "Desserts",
-  //           "Beverages",
-  //         ],
-  //         avgRating: 3.8,
-  //         parentId: "236243",
-  //         avgRatingString: "4.5",
-  //         totalRatingsString: "10K+",
-  //         sla: {
-  //           deliveryTime: 26,
-  //         },
-  //       },
-  //     },
-  //     {
-  //       info: {
-  //         id: "201229",
-  //         name: "KFC",
-  //         cloudinaryImageId: "n15vckntsiboiod3gpco",
-  //         locality: "HSR Layout",
-  //         areaName: "Hsr Layout",
-  //         costForTwo: "₹200 for two",
-  //         cuisines: [
-  //           "South Indian",
-  //           "North Indian",
-  //           "Chinese",
-  //           "Desserts",
-  //           "Beverages",
-  //         ],
-  //         avgRating: 4.1,
-  //         parentId: "236243",
-  //         avgRatingString: "4.5",
-  //         totalRatingsString: "10K+",
-  //         sla: {
-  //           deliveryTime: 26,
-  //         },
-  //       },
-  //     },
-  //   ];
-
-  return (
+  return listofRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setsearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const filteredRestaurant = listofRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="fillter-btn"
           onClick={() => {
@@ -119,7 +72,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listofRestaurants.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
